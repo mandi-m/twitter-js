99088fcb2af288177efe6f9b52b714922742252f
Madeliene Owen <madeliene.owen@gmail.com>
2017-03-07 09:42:14 -0800

    Added a madeliene-comment

--- app.js
+++ app.js
@@ -5,18 +5,23 @@ const app = express();  //creates an instance of the express application, develo
 //can combine the lines
 //const app = require('express')();
 app.use('/', function(request, response, next) {
-  console.log('HTTP verb: ' + request.method + ' route: ' + request.route);
+  console.log('HTTP verb: ' + request.method + ' route: ' + request.originalUrl);
+  next();
 });
 
-app.get('/', function (request, response, next){
-  // console.log(request.params.pupID);
-  console.log(request.method)
-  response.send('<h3>Welcome to Mandi and Madeleine\'s New Page</h3>');
-  //mandi change
-})
+app.use('/special', function(request, response, next) {
+  console.log('Reached special');
+  next();
+});
 
+app.use('/', function (request, response, next){
+  // console.log(request.params.pupID);
+  console.log(request.method);
+  response.send('<h3>Welcome to Mandi and Madeleine\'s New Page</h3>');
+});
 
 app.listen(3000, function(){
   console.log('Hello, listening for requests on port 3000');
 });
 
+// madeliene's change comment

7a5ac66447f71effdd9636b2c0f91d90feffdd34
Madeliene Owen <madeliene.owen@gmail.com>
2017-03-07 11:50:02 -0800

    Nunjucks template addition and test.

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 //will be our main application
 const express = require('express');
+const nunjucks = require('nunjucks');
 const app = express();  //creates an instance of the express application, developers chose to use factory function method instead of new keyword
+
+var locals = {
+    title: 'An Example',
+    people: [
+        { name: 'Gandalf'},
+        { name: 'Frodo' },
+        { name: 'Hermione'}
+    ]
+};
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+//nunjucks.configure('views'); // point nunjucks to the proper directory for templates
+nunjucks.configure('views', {noCache: true});
+nunjucks.render('index.html', locals, function (err, output) {
+  if (err) throw err;
+  console.log(output);
+});
+
 
 //can combine the lines
 //const app = require('express')();
@@ -17,7 +36,9 @@ app.use('/special', function(request, response, next) {
 app.use('/', function (request, response, next){
   // console.log(request.params.pupID);
   console.log(request.method);
-  response.send('<h3>Welcome to Mandi and Madeleine\'s New Page</h3>');
+  //response.send('<h3>Welcome to Mandi and Madeleine\'s New Page</h3>');
+  const people = [{name: 'Mandi'}, {name: 'Madeliene'}, {name: 'Kids'}];
+  response.render( 'index', {title: 'Fullstackers', people: people} );
 });
 
 app.listen(3000, function(){

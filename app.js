c6f077e291b0f42328e55a2185defea6d6153962
mandi-m <mmeidlinger@yahoo.com>
2017-03-07 14:52:04 -0500

    mandi playing

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 //will be our main application
 const express = require('express');
 const app = express();  //creates an instance of the express application, developers chose to use factory function method instead of new keyword
+const nunjucksMod = require('nunjucks');
+const nunjucks = nunjucksMod();
+
+var locals = {
+    title: 'An Example',
+    people: [
+        { name: 'Gandalf'},
+        { name: 'Frodo' },
+        { name: 'Hermione'}
+    ]
+};
+nunjucks.configure('views', {noCache: true});
+nunjucks.render('index.html', locals, function (err, output) {
+    console.log(output);
+});
+
+
 
 //can combine the lines
 //const app = require('express')();
